5a9851de15623a2d0b756d75ce4c02f393ae661c
Julien Dubois <dubois.julien.mail@gmail.com>
2022-01-30 00:33:06 +0100

    [stowdo] allow frontend in cors policy

--- stowdo/stowdo/settings.py
+++ stowdo/stowdo/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'allauth',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -144,3 +146,14 @@ MINIO_CONFIG = {
     'default_bucket': 'default'
 }
 DEFAULT_FILE_STORAGE = 'storage.minio_storage.MinioStorage'
+
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = [
+        'https://stowdo.tk',
+        'http://localhost:3000',
+    ]
+else:
+    CORS_ALLOWED_ORIGINS = [
+        'https://stowdo.tk',
+    ]
+CORS_ALLOW_CREDENTIALS = True
