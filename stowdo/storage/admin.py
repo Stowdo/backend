1fba3f47670944c73ae0ad32285e7492278113a4
Julien Dubois <dubois.julien.mail@gmail.com>
2022-02-06 19:49:05 +0100

    [storage] add resources to admin panel

--- stowdo/storage/admin.py
+++ stowdo/storage/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
 from storage.models import File, Folder
+from allauth.socialaccount.models import SocialToken, SocialAccount, SocialApp, EmailAddress
 
-admin.register(Folder)
-admin.register(File)
+
+class FolderAdmin(admin.ModelAdmin):
+    pass
+
+
+class FileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Folder, FolderAdmin)
+admin.site.register(File, FileAdmin)
+
+admin.site.unregister(SocialToken)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(Site)
+admin.site.unregister(EmailAddress)
